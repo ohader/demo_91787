f97eb447cf66af3f8468923caed8baa59765fb5f
Oliver Hader <oliver@typo3.org>
2021-09-13 23:00:59 +0200

    [TASK] Rename JavaScript registration method

--- Resources/Public/JavaScript/Demo.js
+++ Resources/Public/JavaScript/Demo.js
@@ -1,5 +1,5 @@
 define(['TYPO3/CMS/Backend/FormEngine', 'TYPO3/CMS/Backend/Modal'], (FormEngine, Modal) => {
-    FormEngine.registerOnFieldChangeHandlers('demo-91787-handler', (data) => {
+    FormEngine.registerOnFieldChangeHandler('demo-91787-handler', (data) => {
         Modal.confirm(data.title, 'Hello from BEHAVIOR fieldChangeFunc handling...')
             .on('button.clicked', () => Modal.dismiss());
     });
